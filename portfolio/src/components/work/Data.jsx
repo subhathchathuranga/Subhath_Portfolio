55a35ed0c2bfcff1816491f88c22ab892f390f90
Subhath Chathuranga <80580145+subhathchathuranga@users.noreply.github.com>
2025-06-25 08:32:01 +0530

    changes made

--- portfolio/src/components/work/Data.jsx
+++ portfolio/src/components/work/Data.jsx
@@ -3,37 +3,52 @@ import Work2 from "../../assets/work2.jpg"
 import Work3 from "../../assets/work3.jpg"
 import Work4 from "../../assets/work4.jpg"
 import Work5 from "../../assets/work5.jpg"
+import Work6 from "../../assets/foodie.png"
+import Work7 from "../../assets/SportClubApp.png"
+import Work8 from "../../assets/travel.png"
 
 export const projectsData = [
   {
     id: 1,
     image: Work1,
-    title: "Web design",
+    title: "Savory Stays | Web App | 2025",
     category: "web",
+    link: "https://github.com/subhathchathuranga/Savory-Stays",
   },
   {
     id: 2,
-    image: Work2,
-    title: "App movil",
+    image: Work6,
+    title: "Foodie | Mobile App | 2025",
     category: "app",
+    link: "https://github.com/subhathchathuranga/Foodie",
   },
   {
     id: 3,
     image: Work3,
-    title: "Brand design",
-    category: "design",
+    title: "Book Store | Web App | 2025",
+    category: "web",
+    link: "https://github.com/subhathchathuranga/BookStore-Management-Website",
   },
   {
     id: 4,
     image: Work4,
-    title: "App movil",
-    category: "app",
+    title: "Heartbeat Shop | Web App | 2024",
+    category: "web",
+    link: "#",
   },
   {
     id: 5,
-    image: Work5,
-    title: "Web design",
+    image: Work7,
+    title: "UOJ Sports Club | Desktop App | 2023",
+    category: "desktop",
+    link: "https://github.com/subhathchathuranga/Mini_Project-UOJ-sport-club-management-system",
+  },
+  {
+    id: 6,
+    image: Work8,
+    title: "Traveling Guide | Web App | 2023",
     category: "web",
+    link: "https://github.com/subhathchathuranga/travel-website",
   },
 ];
 
@@ -49,6 +64,6 @@ export const projectsNav = [
     name:'app',
   },
   {
-    name:'design',
+    name:'desktop',
   },
 ];
--- portfolio/src/components/work/Work.jsx
+++ portfolio/src/components/work/Work.jsx
@@ -5,7 +5,7 @@ import Works from './Works'
 const work = () => {
   return (
     <section className="work section" id="portfolio">
-        <h2 className="section__title">Portfolio</h2>
+        <h2 className="section__title">Projects</h2>
         <span className="section_subtitle">Most recent works</span>
         <Works/>
     </section>
